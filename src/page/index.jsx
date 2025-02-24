91fca756ee156e6a7086740e2de32376276f301f
Nahal Malik <117668550+nahalmalik@users.noreply.github.com>
2025-02-24 16:36:45 +0500

    Add files via upload

---
+++ src/page/index.jsx
@@ -0,0 +1,230 @@
+import React from "react";
+import "./index.css";
+
+export default function Main() {
+  return (
+    <div className="main-container">
+      <div className="flex-row">
+        <div className="vector" />
+        <div className="regroup">
+          <span className="instructor">Instructor</span>
+          <span className="about-us">About Us</span>
+          <span className="contact">Contact</span>
+        </div>
+        <div className="rectangle">
+          <span className="start-learning">Start Learning</span>
+        </div>
+        <span className="mentor-craft">MENTOR CRAFT</span>
+        <span className="home-1">Home</span>
+        <span className="all-courses">All Courses</span>
+      </div>
+      <span className="learning-mind">“Learning never exhausts the mind.”</span>
+      <span className="lorem-ipsum">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+        pellentesq. Duis et egestas nunc. In ornare, nunc quis commodo
+        tincidunt, dui augue , id iaculis .
+      </span>
+      <div className="rectangle-2">
+        <span className="view-all-courses">View All Courses</span>
+      </div>
+      <div className="flex-row-da">
+        <div className="feature">
+          <div className="vector-3" />
+          <span className="actionable-training">Actionable Training</span>
+          <span className="lorem-ipsum-4">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed.
+          </span>
+        </div>
+        <div className="feature-5">
+          <div className="vectro" />
+          <span className="interesting-quizzes">Interesting Quizzes</span>
+          <span className="lorem-ipsum-6">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed.
+          </span>
+        </div>
+        <div className="feature-7">
+          <div className="vector-8" />
+          <span className="premium-material">Premium Material</span>
+          <span className="lorem-ipsum-9">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed.
+          </span>
+        </div>
+      </div>
+      <div className="flex-row-f">
+        <span className="popular-courses">Our Most Popular Courses</span>
+        <span className="lorem-ipsum-a">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
+        </span>
+        <div className="courses-bg" />
+        <div className="course">
+          <div className="empty" />
+          <span className="course-name">Course Name</span>
+          <span className="lorem-ipsum-b">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          </span>
+          <div className="rectangle-c">
+            <span className="see-more">See More......</span>
+          </div>
+        </div>
+        <div className="course-d">
+          <div className="empty-e" />
+          <span className="course-name-f">Course Name</span>
+          <span className="lorem-ipsum-10">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          </span>
+          <div className="rectangle-11">
+            <span className="see-more-12">See More......</span>
+          </div>
+        </div>
+        <div className="course-13">
+          <div className="empty-14" />
+          <span className="course-name-15">Course Name</span>
+          <span className="lorem-ipsum-16">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          </span>
+          <div className="rectangle-17">
+            <span className="see-more-18">See More......</span>
+          </div>
+        </div>
+      </div>
+      <div className="rectangle-19">
+        <div className="images">
+          <span className="underscore">_ _ _ _ _ _</span>
+          <div className="rectangle-1a">
+            <span className="study-pace">Study at your own pace</span>
+            <span className="boost-career">
+              Boost your career by learning skills in high demand
+            </span>
+            <div className="rectangle-1b">
+              <span className="get-started">Get Started</span>
+            </div>
+            <div className="arrow" />
+          </div>
+          <span className="underscore-1c">_ _ _ _ _ _</span>
+          <span className="underscore-1d">_ _ _ _ _ _</span>
+          <span className="education">Education</span>
+          <span className="experience">Experience</span>
+          <span className="certificate">Certificate</span>
+          <span className="lorem-ipsum-1e">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.{" "}
+          </span>
+          <span className="lorem-ipsum-1f">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.{" "}
+          </span>
+          <span className="lorem-ipsum-20">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.{" "}
+          </span>
+          <div className="rectangle-21">
+            <span className="see-more-22">See More ....</span>
+          </div>
+          <div className="rectangle-23">
+            <span className="see-more-24">See More ....</span>
+          </div>
+          <div className="rectangle-25">
+            <span className="see-more-26">See More ....</span>
+          </div>
+        </div>
+      </div>
+      <div className="rectangle-27">
+        <span className="student-testimonials">Student Testimonials</span>
+        <div className="flex-row-28">
+          <div className="rectangle-29">
+            <div className="rating" />
+            <span className="testimonial-text">
+              “Massa amet, at dolor tellus pellentesque aenean in eget massa
+              tincidunt habitasse volutpat adipiscing sed id sit auctor eu
+              vivamus nulla.”
+            </span>
+            <div className="image" />
+            <span className="author-name">Emma Hart</span>
+          </div>
+          <div className="rectangle-2a">
+            <div className="rating-2b" />
+            <span className="testimonial-text-2c">
+              “Massa amet, at dolor tellus pellentesque aenean in eget massa
+              tincidunt habitasse volutpat adipiscing sed id sit auctor eu
+              vivamus nulla.”
+            </span>
+            <div className="user-info" />
+            <span className="author-name-2d">Eddie Johnson</span>
+          </div>
+        </div>
+        <div className="flex-row-2e">
+          <div className="rectangle-2f">
+            <div className="rating-30" />
+            <span className="testimonial-text-31">
+              “Massa amet, at dolor tellus pellentesque aenean in eget massa
+              tincidunt habitasse volutpat adipiscing sed id sit auctor eu
+              vivamus nulla.”
+            </span>
+            <div className="user-info-32" />
+            <span className="author-name-33">Jonathan Doe</span>
+          </div>
+          <div className="rectangle-34">
+            <div className="rating-35" />
+            <span className="testimonial-text-36">
+              “Massa amet, at dolor tellus pellentesque aenean in eget massa
+              tincidunt habitasse volutpat adipiscing sed id sit auctor eu
+              vivamus nulla.”
+            </span>
+            <div className="user-info-37" />
+            <span className="author-name-38">Lisa Hallway</span>
+          </div>
+        </div>
+      </div>
+      <div className="flex-row-39">
+        <span className="join-community">Join Our Community</span>
+        <span className="newsletter-subscription">
+          Enter your email address to register to our Newsletter Subscription
+          delivered on regular basis
+        </span>
+        <div className="rectangle-3a" />
+        <div className="rectangle-3b">
+          <span className="subscribe">Subscribe</span>
+        </div>
+        <div className="rectangle-3c">
+          <span className="enter-email">Enter Your Email</span>
+        </div>
+      </div>
+      <div className="rectangle-3d">
+        <div className="flex-row-3e">
+          <div className="vector-3f" />
+          <span className="popular-courses-40">Popular Courses</span>
+          <span className="contact-info">Contact Info</span>
+          <span className="mentor-craft-41">MENTOR CRAFT</span>
+        </div>
+        <div className="flex-row-cd">
+          <span className="course-list">
+            Course no 1<br />
+            Course no 2<br />
+            Course no 3<br />
+            Course no 4<br />
+            Course no 5
+          </span>
+          <span className="address-info">
+            Address:
+            <br /> ---------------------------
+            <br />
+            Phone No:
+            <br /> 0000-0000000
+            <br />
+            Email:
+            <br /> abc@xyz.com
+          </span>
+          <span className="lorem-ipsum-42">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
+            suscipit euismod sapien
+          </span>
+          <div className="whatsapp-icon" />
+          <div className="facebook-icon" />
+          <div className="youtube-icon" />
+          <div className="group" />
+        </div>
+      </div>
+      <span className="copyright">Copyright @ Mentor-Craft Team </span>
+      <div className="header" />
+      <div className="center-bg" />
+    </div>
+  );
+}
